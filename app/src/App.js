91bb0e3e676e0ec18b8efbaee343395ce7df1bb0
Bala <57966220+sjbavayy241@users.noreply.github.com>
2023-03-19 22:16:47 +0100

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="title">
-          I am an Example App Running in the Cloud FOR MR. BALU
+          I am an Example App Running in the Cloud FOR MR.Bala
         </div>
       </div>
     );
